d4764e37898203582324b71c4510c3c16c399ef3
Sumant17 <sumantrkurse@gmail.com>
2024-06-17 11:28:22 +0530

    add friends

--- lib/models/friends_list_model.dart
+++ lib/models/friends_list_model.dart
@@ -2,4 +2,14 @@ class FriendsListModel {
   final String friendname;
 
   FriendsListModel({required this.friendname});
+
+  factory FriendsListModel.fromJson(Map<String, dynamic> json) {
+    return FriendsListModel(friendname: json['friendname']);
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      "friendname": friendname,
+    };
+  }
 }
